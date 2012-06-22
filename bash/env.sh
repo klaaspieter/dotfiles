52fde22103bfa399246026e8cf5adb86223b95a9
Klaas Pieter Annema <klaaspieter@annema.me>
2012-06-22 14:20:13 -0400

    fix 256 color support

--- bash/env.sh
+++ bash/env.sh
@@ -3,4 +3,5 @@ export GIT_EDITOR=`which vim`
 
 export CLICOLOR=1 # Turn colors on
 export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
-export TERM=xterm-256color # Enable 256 color support
+
+[ "$TERM" = "xterm" ] && TERM="xterm-256color"
