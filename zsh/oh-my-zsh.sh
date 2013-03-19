53c6c00789760cc437692c4e4c361bdadd2eeb21
Klaas Pieter Annema <klaaspieter@annema.me>
2013-03-19 11:07:09 +0100

    Disable zsh theme and use powerline

--- zsh/oh-my-zsh.sh
+++ zsh/oh-my-zsh.sh
@@ -1,4 +1,4 @@
 ZSH=$HOME/.oh-my-zsh
-ZSH_THEME="garyblessington"
+ZSH_THEME=""
 plugins=(git brew bundler osx)
 source $ZSH/oh-my-zsh.sh
--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -1,2 +1,3 @@
 autoload -U colors && colors
 export SPROMPT="Correct $fg[red]%R$reset_color to $fg[green]%r?$reset_color [nyae]? "
+source ~/.powerline/powerline/bindings/zsh/powerline.zsh
