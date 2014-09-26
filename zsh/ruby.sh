427d62b126b0f0c054bba2aa9ec7fb75089ff2f3
Klaas Pieter Annema <klaaspieter@annema.me>
2014-09-26 11:57:54 +0200

    Update ruby

--- zsh/ruby.sh
+++ zsh/ruby.sh
@@ -1,26 +1,4 @@
-#Add the following to the ~/.bashrc or ~/.zshrc file:
-#
-source /usr/local/opt/chruby/share/chruby/chruby.sh
-#
-#By default chruby will search for Rubies installed into /opt/rubies/ or
-#~/.rubies/. For non-standard installation locations, simply set the RUBIES
-#variable:
-#
-#  RUBIES=(
-#    /opt/jruby-1.7.0
-#    $HOME/src/rubinius
-#  )
-#
-#If you are migrating from another Ruby manager, set `RUBIES` accordingly:
-#
-#  RVM:   RUBIES=(~/.rvm/rubies/*)
-#  rbenv: RUBIES=(~/.rbenv/versions/*)
-#  rbfu:  RUBIES=(~/.rbfu/rubies/*)
-#
-#To enable auto-switching of Rubies specified by .ruby-version files,
-#add the following to ~/.bashrc or ~/.zshrc:
-#
-source /usr/local/opt/chruby/share/chruby/auto.sh
+source /usr/local/share/chruby/chruby.sh
+source /usr/local/share/chruby/auto.sh
 
-# Set the default ruby, auto swithcing doesn't work on encrytped disk images
-chruby ruby-2.0
+chruby 2.1.2
