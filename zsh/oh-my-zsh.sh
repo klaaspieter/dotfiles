7f0cb66816bea59b6cb998074be930b5419546f8
Klaas Pieter Annema <klaaspieter@annema.me>
2014-11-13 10:30:29 -0500

    More zsh plugins!

--- zsh/oh-my-zsh.sh
+++ zsh/oh-my-zsh.sh
@@ -1,4 +1,4 @@
 ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="agnoster"
-plugins=(git gitfast git-extras brew bundler osx)
+plugins=(git gitfast git-extras brew bundler osx chruby gem gradle pod powder)
 source $ZSH/oh-my-zsh.sh
