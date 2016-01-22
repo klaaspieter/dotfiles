0f2367254f94cfd48bac99c50588efd90f5f80d3
Klaas Pieter Annema <kpa@annema.me>
2016-01-22 17:20:10 +0100

    Configure gpg

--- zsh/env.sh
+++ zsh/env.sh
@@ -1,3 +1,11 @@
 export EDITOR='vim'
 export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
+
+[ -f ~/.gpg-agent-info ] && source ~/.gpg-agent-info
+if [ -S "${GPG_AGENT_INFO%%:*}" ]; then
+  export GPG_AGENT_INFO
+  export GPG_TTY=$(tty)
+else
+  eval $( gpg-agent --daemon --write-env-file ~/.gpg-agent-info )
+fi
