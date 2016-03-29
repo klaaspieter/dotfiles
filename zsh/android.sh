7d2fc991d0e8227ebe4c9eca6b132772d17ae831
Klaas Pieter Annema <kpa@annema.me>
2016-03-29 09:24:42 +0200

    Update Android environment

--- zsh/android.sh
+++ zsh/android.sh
@@ -1,1 +1,5 @@
-export ANDROID_HOME="/usr/local/opt/android-sdk"
+export ANT_HOME=/usr/local/opt/ant
+export MAVEN_HOME=/usr/local/opt/maven
+export GRADLE_HOME=/usr/local/opt/gradle
+export ANDROID_HOME=/usr/local/opt/android-sdk
+export ANDROID_NDK_HOME=/usr/local/opt/android-ndk
