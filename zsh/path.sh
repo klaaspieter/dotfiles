8f2b444eb383ca8b623c8d688ce29f3d7e073b4b
Klaas Pieter Annema <kpa@annema.me>
2016-04-07 13:24:31 +0200

    Add Android paths

--- zsh/path.sh
+++ zsh/path.sh
@@ -1,3 +1,13 @@
 export PATH=/usr/local/bin:/usr/local/sbin:~/.bin:$PATH
+
+# Go
 export GOPATH=$HOME/Developer/golang
 export PATH=$PATH:$GOPATH/bin
+
+# Android
+export PATH=$ANT_HOME/bin:$PATH
+export PATH=$MAVEN_HOME/bin:$PATH
+export PATH=$GRADLE_HOME/bin:$PATH
+export PATH=$ANDROID_HOME/tools:$PATH
+export PATH=$ANDROID_HOME/platform-tools:$PATH
+export PATH=$ANDROID_HOME/build-tools/19.1.0:$PATH
