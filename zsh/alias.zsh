86f8049cdc4127ea92163fd5a50f88a4edde465d
Klaas Pieter Annema <kpa@annema.me>
2016-11-14 13:08:14 +0100

    Rename nuke to xcnuke
    Because most Xcode related commands are prefixed with xc.

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -7,5 +7,6 @@ alias gdf="git difftool"
 alias ll="ls -lh"
 
 alias p="pod install"
-alias nuke="rm -rf ~/Library/Developer/Xcode/DerivedData"
 alias sl="git diff --name-only master | ack \".swift\" | xargs -L1 swiftlint lint --path 2>/dev/null"
+
+alias xcnuke="rm -rf ~/Library/Developer/Xcode/DerivedData"
