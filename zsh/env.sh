096dbac52d72c63ba5fbf97a68cc2d68bb89c2f0
Klaas Pieter Annema <kpa@annema.me>
2017-04-25 07:25:58 +0200

    Remove gpg-agent configuration
    Upgraded to gpg2.1 which includes it's own gpg-agent.

--- zsh/env.sh
+++ zsh/env.sh
@@ -5,11 +5,4 @@ export EDITOR="vim"
 bindkey -e
 export KEYTIMEOUT=1
 
-[ -f ~/.gpg-agent-info ] && source ~/.gpg-agent-info
-if [ -S "${GPG_AGENT_INFO%%:*}" ]; then
-  export GPG_AGENT_INFO
-  export GPG_TTY=$(tty)
-else
-  eval $( gpg-agent --daemon --write-env-file ~/.gpg-agent-info )
-fi
 export GPGKEY=62C3F535
