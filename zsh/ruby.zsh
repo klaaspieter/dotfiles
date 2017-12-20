fa89dd8a1739b0adf4c35bc92a24cd4e1875e9cf
Klaas Pieter Annema <kpa@annema.me>
2017-12-20 14:07:17 +0100

    Remove chruby in favor of asdf

--- zsh/ruby.zsh
+++ zsh/ruby.zsh
@@ -1,8 +1,3 @@
-source /usr/local/share/chruby/chruby.sh
-source /usr/local/share/chruby/auto.sh
-
-chruby 2.3.3
-
 alias be="bundle exec"
 alias b="bundle install -j4"
 alias rr="mkdir -p tmp && touch tmp/restart.txt"
