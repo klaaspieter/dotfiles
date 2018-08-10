908fb1c00dbfc8c11ea88371c2b2525d91f1a741
Klaas Pieter Annema <kp@annema.me>
2018-08-10 11:37:17 +0200

    Configure hammerspoon

---
+++ hammerspoon/init.lua
@@ -0,0 +1,44 @@
+-- Automatically reload configuration
+hs.loadSpoon("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
+
+-- Disable window animations
+hs.window.animationDuration = 0
+
+-- I use vim, thus I prefer modal keybindings.
+-- This also corresponds to my old Divvy keybinding.
+-- This configures alt+space to enter normal mode.
+local normal = hs.hotkey.modal.new({'alt'}, 'space')
+
+normal.entered = function()
+  -- hs.alert.show('Normal mode')
+end
+
+normal.exited = function()
+  -- hs.alert.show('Insert mode')
+end
+
+function exitModal(action)
+  return function()
+    action()
+    normal:exit()
+  end
+end
+
+
+-- Center the focused window
+local centerWindow = exitModal(function()
+  local window = hs.window.focusedWindow()
+  local screenFrame = window:screen():frame()
+
+  local newFrame = screenFrame:scale(0.7)
+
+  window:setFrame(newFrame)
+end)
+normal:bind({}, 'c', centerWindow)
+
+-- Maximize the focused window
+local fullScreenWindow = exitModal(function()
+  hs.window.focusedWindow():maximize()
+end)
+normal:bind({}, 'f', fullScreenWindow)
