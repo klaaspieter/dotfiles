8a6eff21a58584e9244e9ce4ddcbb83be6e61284
Klaas Pieter Annema <kp@annema.me>
2023-04-03 11:55:33 +0200

    Add surround.nvim

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -6,5 +6,5 @@ return {
     config = function()
       vim.cmd([[colorscheme gruvbox8_hard]])
     end,
-  }
+  },
 }
---
+++ config/nvim/lua/plugins/surround.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+      })
+    end,
+  },
+}
