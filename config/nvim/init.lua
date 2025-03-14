4a595d88489fae09c4c514298e9268527a181d3d
Klaas Pieter Annema <kp@annema.me>
2025-03-14 17:54:33 +0100

    Setup LazyVim
    I've been using VSCode for a while but it's starting to show the  same issues I
    ended up switching for.
    
    My own nvim setup was already inspired by LazyVim. This cuts out the middle man
    (me) and uses LazyVim directly.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,22 +1,2 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-local set = vim.opt
-
-set.number = true
-set.tabstop = 2
-set.shiftwidth = 0
-set.expandtab = true
-set.smartindent = true
-set.undofile = true
-set.termguicolors = true
-
-set.list = true
-set.listchars = { tab = "▸ ", eol = "¬", trail = "." }
-
-set.splitbelow = true
-set.splitright = true
-
--- vim.g.gruvbox_contrast_dark = "hard"
--- vim.g.gruvbox_contrast_light = "medium"
-
-vim.keymap.set("n", "-", vim.cmd.Ex)
