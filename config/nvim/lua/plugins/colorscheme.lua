cbf0583d6570a12b23f6c5f2cb4aa8b756c4057f
Klaas Pieter Annema <kp@annema.me>
2025-11-14 16:23:47 +0100

    Experiment with Alabaster color scheme
    I agree with the author that most color schemes are christmas lights diarrhea.
    Eventually I intend to make my own scheme inspired by Alabaster but before I go
    all in I want to try Alabaster first.
    
    See https://tonsky.me/blog/syntax-highlighting/

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "https://git.sr.ht/~p00f/alabaster.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "alabaster",
+    },
+  },
+}
