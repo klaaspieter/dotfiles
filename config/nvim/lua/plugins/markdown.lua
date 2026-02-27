717b83e2b5cc9bcb36b45b59f7ee9db71cec4674
Klaas Pieter Annema <kp@annema.me>
2026-02-27 14:59:26 +0100

    Improve LazyVim Markdown formatting and linting
    Enable common options to better support GitHub flavored markdown. Also prevent
    prettier from formatting markdown documents because it has known
    incompatibilities with no reasonable workarounds.

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -15,7 +15,18 @@ return {
       formatters = {
         ["markdownlint-cli2"] = {
           args = { "--config", vim.fn.expand("$HOME/.markdownlint-cli2.yaml"), "--fix", "$FILENAME" },
+
+          -- Override LazyVim's condition
+          -- Prevent falling back to prettier (in projects where it's
+          -- configured) when there are no markdownlint failures.
+          condition = function()
+            return true
+          end,
         },
+      },
+      formatters_by_ft = {
+        markdown = { "markdownlint-cli2", "markdown-toc" },
+        ["markdown.mdx"] = { "markdownlint-cli2", "markdown-toc" },
       },
     },
   },
